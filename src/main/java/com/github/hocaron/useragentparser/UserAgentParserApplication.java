0a744fd3cd06e22b0f0bf5ffca1c1678679308b9
hocaron <kkannu0407@gmail.com>
2024-04-13 02:56:22 +0900

    init: project

---
+++ src/main/java/com/github/hocaron/useragentparser/UserAgentParserApplication.java
@@ -0,0 +1,13 @@
+package com.github.hocaron.useragentparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAgentParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAgentParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hocaron/useragentparser/UserAgentParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hocaron.useragentparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAgentParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
